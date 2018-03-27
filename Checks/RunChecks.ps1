aa8443c66563e8a539a5239d939971806d1dde2c
Timothy Addison <git@tjaddison.com>
2018-03-27 19:43:36 +0100

    Change config naming

--- Checks/RunChecks.ps1
+++ Checks/RunChecks.ps1
@@ -1,20 +1,20 @@
 #Requires -Module dbachecks
 
-Get-Content -Path './config.json' -Raw | ConvertFrom-Json -OutVariable globalConfig
+Get-Content -Path '.\global.config.json' -Raw | ConvertFrom-Json -OutVariable globalConfig
 
 $resultExportPath = $globalConfig.ResultExportPath
 
 foreach($instance in Get-ChildItem -Path './Environments' -Directory)
 {
-    $dbcConfigPath = $instance.FullName + "\DbcConfig.json"
+    $dbcConfigPath = $instance.FullName + "\dbachecks.config.json"
     Import-DbcConfig -Path $dbcConfigPath
 
-    $testConfigPath = $instance.FullName + "\TestConfig.json"
+    $testConfigPath = $instance.FullName + "\tests.config.json"
     Get-Content -Path $testConfigPath -Raw | ConvertFrom-Json -OutVariable testConfig
 
     $environment = $testConfig.Environment
     
-    foreach($check in $testConfig.Checks)
+    foreach($check in $testConfig.DBAChecks.Checks)
     {
         Invoke-DbcCheck -Check $check -PassThru | Update-DbcPowerBiDataSource -Environment $environment -Path $resultExportPath
     }   
