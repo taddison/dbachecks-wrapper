c8bfe9dc9de40c7140a50cc1214f5e463a0bb56d
Timothy Addison <git@tjaddison.com>
2018-03-27 20:56:40 +0100

    Big SQLChecks refactor

--- Checks/RunChecks.ps1
+++ Checks/RunChecks.ps1
@@ -1,23 +1,55 @@
-#Requires -Module dbachecks
+#Requires -Modules dbachecks, SQLChecks
 
-Get-Content -Path '.\global.config.json' -Raw | ConvertFrom-Json -OutVariable globalConfig
+Get-Content -Path '.\global.config.json' -Raw | ConvertFrom-Json -OutVariable globalConfig | Out-Null
 
 $resultExportPath = $globalConfig.ResultExportPath
+$sqlChecksTestPath = $globalConfig.SQLChecksTestPath
 
 foreach($instance in Get-ChildItem -Path './Environments' -Directory)
 {
-    $dbcConfigPath = $instance.FullName + "\dbachecks.config.json"
-    Import-DbcConfig -Path $dbcConfigPath
-
+    # Global config settings
     $testConfigPath = $instance.FullName + "\tests.config.json"
-    Get-Content -Path $testConfigPath -Raw | ConvertFrom-Json -OutVariable testConfig
+    Get-Content -Path $testConfigPath -Raw | ConvertFrom-Json -OutVariable testConfig | Out-Null
 
     $environment = $testConfig.Environment
-    
-    foreach($check in $testConfig.DBAChecks.Checks)
+
+    # DBAChecks
+    if(Test-Path ($instance.FullName + "\dbachecks.config.json"))
     {
-        Invoke-DbcCheck -Check $check -PassThru | Update-DbcPowerBiDataSource -Environment $environment -Path $resultExportPath
-    }   
+        Write-Host "Found DBAChecks configuration for $environment"
+
+        $dbcConfigPath = $instance.FullName + "\dbachecks.config.json"
+        Import-DbcConfig -Path $dbcConfigPath | Out-Null
+        
+        foreach($check in $testConfig.DBAChecks.Checks)
+        {
+            Write-Host "[DBACHECKS] Running $check on $environment"
+            Invoke-DbcCheck -Check $check -PassThru | Update-DbcPowerBiDataSource -Environment $environment -Path $resultExportPath
+        }   
+    }
+
+    $sqlChecksFolder = $instance.FullName + "\\SQLChecks"
+    if(Test-Path ($sqlChecksFolder))
+    {
+        Write-Host "Found SQLChecks folder for $environment"
+
+        foreach($sqlChecksConfig in Get-ChildItem -Path $sqlChecksFolder -Filter "*.config.json")
+        {
+            $sqlChecksConfigPath = $sqlChecksConfig.FullName
+            [string]$data = Get-Content -Path $sqlChecksConfigPath -Raw
+            $data | ConvertFrom-Json -OutVariable sqlCheckConfig | Out-Null
+
+            $instance = $sqlCheckConfig.ServerInstance
+            $sqlCheckEnvironment = $environment + "-" + $instance
+            $exportPath = $resultExportPath + "\\SQLChecks\\$environment"
+            
+            foreach($check in $sqlCheckConfig | Get-Member -Type NoteProperty | Where-Object { $_.Name -ne "ServerInstance"} | Select-Object -ExpandProperty Name)
+            {
+                Write-Host "[SQLCHECKS] Running $check on $environment - $instance"
+                Invoke-Pester -Script @{Path=$sqlChecksTestPath;Parameters= @{configs=$sqlChecksConfig}} -Tag $check -PassThru | Update-DbcPowerBiDataSource -Environment $sqlCheckEnvironment -Path $exportPath
+            } 
+        }        
+    }
 }
 
-Start-DbcPowerBi -Path ../Dashboard/
+#Start-DbcPowerBi -Path ../Dashboard/
--- README.md
+++ README.md
@@ -1,22 +1,22 @@
 # dbachecks-wrapper
-An example of how you might wrap [dbachecks](https://github.com/sqlcollaborative/dbachecks).
+An example of how you might wrap [dbachecks](https://github.com/sqlcollaborative/dbachecks) and get tests you've written elsewhere (in this example [SQLChecks](https://github.com/taddison/SQLChecks)) reported on the DBAChecks dashboard.
 
 The code to execute the checks and read config values is all contained in [RunAllChecks.ps1](https://github.com/taddison/dbachecks-wrapper/blob/master/Checks/RunChecks.ps1).
 
 ## Structure
 - A root folder to contain all global settings and invocation scripts
-  - Right now the only global setting is the export path
-- A subfolder containing one folder per environment to check
-- A pair of config files per-environment, with details about the environment as well as the specific DbcConfig settings to import
-  - Multiple servers can be configured per-environment
-- The intention is the checks folder is source controlled - the Results/Power BI folder would not be
+  - Right now the global settings are the export path (for the json files used by the Power BI dashboard), and the path to SQLChecks
+- A subfolder called Environments, with one folder per environment
+- Each environment folder contains a pair of config files, with details about the environment as well as the dbachecks config settings
+- An optional folder SQLChecks (per-environment), which contains any SQLChecks config files - one per-instance in the environment
 
-## Example
-- In the example we run three sets of checks against the localhost instance
-  - Prod has two servers (locahost and 127.0.0.1), and dev has one server (::1)
-- One environment (PROD) is set to check DAC is on, and the other (DEV) that DAC is off (one will always fail)
-- Both environments run the suspect page check
-- One file per-check per-environment will be produced in the specified output folder
-- The PowerBI dashboard is launched - click refresh to update the results
+## The Script
+- The script will execute all DBAChecks specified in the tests.config.json file for the environment
+- The script will also execute any SQLChecks tests found, only attempting to execute tests which are present in the configuration file
+
+## TODO
+- The SQLChecks tests are currently exported as ENVIRONMENT-SERVER, and need to be merged into a single results file
+- SQLChecks doesn't currently have any tags and so no tests ever run (without hacking the repo)
+- SQLChecks doesn't use Describe blocks in the way DBAChecks expects, and they need to be refactored to have the last word be the ServerInstance
 
 ![Dashboard](/img/DashboardExample.png)
